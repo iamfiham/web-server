1e2b5f40a71cbac1879fb05f58cc4950f8ab1eeb
Mohamed Fiham <xyzfiham@gmail.com>
2025-08-15 15:33:25 +0530

    code refactor

--- src/main.cpp
+++ src/main.cpp
@@ -6,6 +6,8 @@
 #include <vector>
 #include <memory>
 
+#include <string.h>
+
 #include <response.hpp>
 
 #define PRINT_LINE(x) std::cout << x << '\n';
@@ -37,73 +39,97 @@ std::string get_content_type(const std::string &path)
 	return "application/octet-stream";
 };
 
+void manageRequest(int client_socket)
+{
+	std::string request_buffer;
+	request_buffer.resize(4000);
+
+	int received_bytes = recv(client_socket, request_buffer.data(), request_buffer.capacity(), 0);
+	request_buffer.resize(received_bytes);
+
+	auto find_the_requested_file = [&client_socket, &request_buffer]()
+	{
+		int start_index = request_buffer.find('/');
+		int end_index = request_buffer.find(' ', start_index);
+
+		std::string path = request_buffer.substr(start_index + 1, end_index - start_index - 1);
+
+		if (path.empty())
+		{
+			path = "index.html";
+		}
+
+		std::string content_type = get_content_type(path);
+
+		PRINT_LINE("--------------------------");
+
+		Response response_obj(path, content_type);
+
+		if (response_obj.success)
+		{
+			send(client_socket, response_obj.buffer_ptr(), response_obj.buffer_size(), 0);
+		}
+	};
+
+	if (received_bytes >= 0)
+	{
+		request_buffer.resize(received_bytes);
+		PRINT_LINE(request_buffer);
+
+		find_the_requested_file();
+	}
+
+	close(client_socket);
+}
+
+struct ManageSocket
+{
+	int _socket;
+	sockaddr_in server_address;
+
+	ManageSocket(int port = 8080, in_addr_t addr = INADDR_ANY)
+	{
+		_socket = socket(AF_INET, SOCK_STREAM, 0);
+
+		server_address.sin_family = AF_INET;
+		server_address.sin_port = htons(port);
+		server_address.sin_addr.s_addr = addr;
+
+		bind_socket();
+		listen_socket();
+	}
+
+	void bind_socket()
+	{
+		if (bind(_socket, reinterpret_cast<struct sockaddr *>(&server_address), sizeof(server_address)))
+		{
+			std::cerr << "[ERROR: binding error] " << strerror(errno) << '\n';
+			close(_socket);
+			exit(EXIT_FAILURE);
+		}
+	}
+	void listen_socket()
+	{
+		if (listen(_socket, 5) == -1)
+		{
+			std::cerr << "[ERROR: listening error] " << strerror(errno) << '\n';
+			close(_socket);
+			exit(EXIT_FAILURE);
+		}
+	}
+};
+
 int main()
 {
-	int web_server_socket = socket(AF_INET, SOCK_STREAM, 0);
-
-	sockaddr_in server_address;
-	server_address.sin_family = AF_INET;
-	server_address.sin_port = htons(8080);
-	server_address.sin_addr.s_addr = INADDR_ANY;
-
-	if (bind(web_server_socket, reinterpret_cast<struct sockaddr *>(&server_address), sizeof(server_address)))
-	{
-		std::cerr << "ERROR: binding error" << '\n';
-		return 1;
-	}
-
-	if (listen(web_server_socket, 5) == -1)
-	{
-		std::cerr << "ERROR: listening error" << '\n';
-		return 1;
-	}
+	ManageSocket web_socket(INADDR_ANY, 8080);
 
 	while (true)
 	{
-		int client_socket = accept(web_server_socket, nullptr, nullptr);
+		int client_socket = accept(web_socket._socket, nullptr, nullptr);
 
 		if (client_socket >= 0)
 		{
-			std::string request_buffer;
-			request_buffer.resize(4000);
-
-			int received_bytes = recv(client_socket, request_buffer.data(), request_buffer.capacity(), 0);
-			request_buffer.resize(received_bytes);
-
-			auto find_the_requested_file = [&client_socket, &request_buffer]()
-			{
-				int start_index = request_buffer.find('/');
-				int end_index = request_buffer.find(' ', start_index);
-
-				std::string path = request_buffer.substr(start_index + 1, end_index - start_index - 1);
-
-				if (path.empty())
-				{
-					path = "index.html";
-				}
-
-				std::string content_type = get_content_type(path);
-				PRINT_LINE(path);
-				PRINT_LINE(content_type);
-				PRINT_LINE("--------------------------");
-
-				Response response_obj(path, content_type);
-
-				if (response_obj.success)
-				{
-					send(client_socket, response_obj.buffer_ptr(), response_obj.buffer_size(), 0);
-				}
-			};
-
-			if (received_bytes >= 0)
-			{
-				request_buffer.resize(received_bytes);
-				PRINT_LINE(request_buffer);
-
-				find_the_requested_file();
-			}
-
-			close(client_socket);
+			manageRequest(client_socket);
 		}
 		else if (client_socket == -1)
 		{
@@ -111,7 +137,7 @@ int main()
 		}
 	}
 
-	close(web_server_socket);
+	close(web_socket._socket);
 
 	return 0;
 }
